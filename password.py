4f73911f3cfcba4cad7b2b957711f39ac3acecb6
Robert Huang <robert830920@gmail.com>
2020-10-21 12:10:50 +0800

    Upload new file

---
+++ password.py
@@ -0,0 +1,12 @@
+#密碼重試程式
+password = 'a123456'
+x = 3 #剩餘機會
+while x > 0:
+
+    pwd = input('請輸入您的密碼：')
+    if pwd == password:
+        print('登入成功')
+        break
+    else:
+    	x = x - 1
+    	print('密碼錯誤！還有', x, '次機會！')
